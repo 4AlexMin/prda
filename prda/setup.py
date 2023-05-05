827fdb306431d8f1b8eb77cab672cfa1995eab27
alexmin <alexminsj@icloud.com>
2023-05-05 16:04:05 +0800

    enable pip

--- prda/setup.py
+++ prda/setup.py
@@ -3,17 +3,13 @@ from setuptools import setup, find_packages
 with open('LICENSE', 'r') as f:
     license = f.read()
 
-with open('../README.md', 'r') as f:
-    long_description = f.read()
 
 setup(
     name='prda',
-    version='1.0.0',
+    version='1.0.1',
     author='Shaojie Min',
     author_email='alexmin@cqu.edu.cn',
     description='Prda contains packages for data processing, analysis and visualization. The ultimate goal is to fill the “last mile” between analysts and packages.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     url='https://github.com/4AlexMin/prda',
     license=license,
     packages=find_packages(),
